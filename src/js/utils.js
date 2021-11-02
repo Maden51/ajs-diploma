c1ca7db5d4a318e1e7eca82618a6354e06b5ac52
Maden5126 <madenon123@gmail.com>
2021-11-02 13:29:20 +0300

    Добавлена фунция calcTileType и автотест

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,22 @@
 export function calcTileType(index, boardSize) {
     // TODO: write logic here
+    if (index === 0) {
+        return 'top-left';
+    } else if ((index + 1) / boardSize === 1) {
+        return 'top-right';
+    } else if (index > 0 && index < boardSize) {
+        return 'top';
+    } else if (index === boardSize ** 2 - boardSize) {
+        return 'bottom-left';
+    } else if (index + 1 === boardSize ** 2) {
+        return 'bottom-right';
+    } else if (index % boardSize === 0) {
+        return 'left';
+    } else if ((index + 1) % boardSize === 0 ) {
+        return 'right';
+    } else if ((index > boardSize ** 2 - boardSize) && index < boardSize ** 2) {
+        return 'bottom';
+    }
     return 'center';
   }
   
